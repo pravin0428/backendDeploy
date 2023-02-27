eeb98747bfef0c855fbcca9b5957be0ba829f8ff
pravin0428 <mohite46199@gmail.com>
2023-02-27 11:41:01 +0530

    space

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -3,15 +3,15 @@ const UserModel = require("../Schema/user.model");
 const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
 const app = express();
-app.use(express.json())
-let blacklist = []
+app.use(express.json());
+let blacklist = [];
 
 const authMiddlewere = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
-    return res.send("token not found")
+    return res.send("token not found");
   }
-  
+
   try {
     const verification = jwt.verify(token, "SECRET12345");
 
@@ -20,88 +20,79 @@ const authMiddlewere = (req, res, next) => {
     }
 
     if (blacklist.includes(token)) {
-      return res.send("token already used")
+      return res.send("token already used");
     }
-    next()
-
+    next();
   } catch (e) {
-    return res.send(e.message)
+    return res.send(e.message);
   }
-}
-app.get("/", async(req,res)=>{
-  const user = await UserModel.find()
-  res.send(user)
-})
+};
+app.get("/", async (req, res) => {
+  const user = await UserModel.find();
+  res.send(user);
+});
 
 //login up start
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await UserModel.findOne({ email })
-  const passs = await argon2.verify(user.password, password)
+  const user = await UserModel.findOne({ email });
+  const passs = await argon2.verify(user.password, password);
   try {
     if (passs) {
-      const token = jwt.sign({ id: user._id, name: user.name, email: user.email, role: user.role },
+      const token = jwt.sign(
+        { id: user._id, name: user.name, email: user.email, role: user.role },
         "SECRETKEY123",
         {
-          expiresIn: "5 mins"
+          expiresIn: "5 mins",
         }
-      )
+      );
       const refreshToken = jwt.sign({}, "REFRESH", {
-        expiresIn: "7 days"
-      })
-      res.send({ msg: "login sucess", token, refreshToken })
+        expiresIn: "7 days",
+      });
+      res.send({ msg: "login sucess", token, refreshToken });
     }
-    res.status(401).send("invalid user")
+    res.status(401).send("invalid user");
   } catch (e) {
-    return res.send(e.message)
+    return res.send(e.message);
   }
-
-
-})
+});
 
 //sign up start
 
 app.post("/signup", async (req, res) => {
   const { email, password, name, age } = req.body;
 
-  const hash = await argon2.hash(password)
-  
-  const token = req.headers["authorization"]
+  const hash = await argon2.hash(password);
+
+  const token = req.headers["authorization"];
 
   try {
-
     if (token) {
-
       const decoded = jwt.decode(token);
-    
+
       if (decoded) {
         if (decoded.role === "HR") {
-          const user = new UserModel({ name, email, password: hash, age, role: "Employee" })
-          await user.save()
-          return res.status(201).send("Employee creation success")
-        }
-        else {
-          return res.status(403).send("You are not allowed")
+          const user = new UserModel({
+            name,
+            email,
+            password: hash,
+            age,
+            role: "Employee",
+          });
+          await user.save();
+          return res.status(201).send("Employee creation success");
+        } else {
+          return res.status(403).send("You are not allowed");
         }
       }
-
     }
-
-  }
-  catch (e) {
-    return res.send(e.message)
+  } catch (e) {
+    return res.send(e.message);
   }
 
-
- 
-  const user = new UserModel({ name, email, password: hash, age })
-  await user.save()
-  return res.status(201).send("Guests creation success")
- 
-
-})
+  const user = new UserModel({ name, email, password: hash, age });
+  await user.save();
+  return res.status(201).send("Guests creation success");
+});
 module.exports = app;
-
-
-
